233cf2d336ff2268ddc8d1e556a762e7fd1fc06f
Jayam Patel <jayampatel04@gmail.com>
2025-12-16 00:35:31 +0530

    Initial Commit

---
+++ src/main/java/space/jayampatel/otelier/OtelierApplication.java
@@ -0,0 +1,13 @@
+package space.jayampatel.otelier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtelierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/jayampatel/otelier/OtelierApplicationTests.java
@@ -0,0 +1,13 @@
+package space.jayampatel.otelier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
