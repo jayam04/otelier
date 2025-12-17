417cec3723c6439252d5ee7553d293cca1cdf2a9
Jayam Patel <jayampatel04@gmail.com>
2025-12-17 11:34:58 +0530

    Removed Employee Objects

--- src/main/java/space/jayampatel/otelier/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package space.jayampatel.otelier.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import space.jayampatel.otelier.model.Employee;
-
-public interface EmployeeRepository extends MongoRepository<Employee, String> {
-}
